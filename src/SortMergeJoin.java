fd4b84c9e36273e0f911c93c5b91a4bbe536ddaa
arzo <arjamizo@gmail.com>
2014-12-14 19:53:00 +0000

    initial commit

---
+++ src/SortMergeJoin.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class SortMergeJoin implements Join{
+
+	@Override
+	public String getName() {
+		return "Sort Merge Join";
+	}
+	
+	@Override
+	public List<Triple> join(List<Tuple> input1, List<Tuple> input2) {
+		
+        // put your implementation in here
+        
+		return null;
+	}
+}
