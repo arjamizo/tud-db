09cb4a04d97e367f662cf6ebea762ca4da88054e
Artur Z <arjamizo@gmail.com>
2014-12-15 04:45:26 +0100

    added .size() vs exception performance test

--- src/SortMergeJoin.java
+++ src/SortMergeJoin.java
@@ -5,9 +5,59 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 
+class PerfTest {
+	java.util.ArrayList<Runnable> tests = new java.util.ArrayList();
+	java.util.ArrayList<Long> times = new java.util.ArrayList();
+	java.util.ArrayList<String> names = new java.util.ArrayList();
+	public PerfTest addTest(String name, Runnable test) {
+		tests.add(test);
+		names.add(name);
+		return this;
+	}
+	public void perform() {
+		for (Runnable test : tests) {
+			long start = System.currentTimeMillis();
+			test.run();
+			long end = System.currentTimeMillis();
+			
+			times.add(end-start);
+		}
+		int idx = times.indexOf(java.util.Collections.min(times));
+		System.out.printf("Names: \n\t%s\nTims:\n\t%s\n", names, times);
+		System.out.printf("The fastest is: \n\t%s. With time of %d[ms]\n\n", names.get(idx), times.get(idx));
+	}
+}
+
 public class SortMergeJoin implements Join{
 	
 	static {
+		new PerfTest().addTest("finishing iteration by learning max size", new Runnable() {
+
+			List<String> list = Arrays.asList((new String(new char[10000]).replace("\0", "a")).split("a"));
+			String str = "";
+			
+			public void run() {
+				for (int i = 0; i < list.size(); i++) {
+					str += list.get(i);
+				}
+				System.out.println(str.length());
+			}
+		}).addTest("finishing iteration by occurence of an exception", new Runnable() {
+
+			List<String> list = Arrays.asList((new String(new char[10000]).replace("\0", "a")).split("a"));
+			String str = "";
+			
+			public void run() {
+				try {
+					for (int i = 0; ; i++) {
+						str += list.get(i);
+					}
+				} catch (ArrayIndexOutOfBoundsException e) {
+					System.out.println(str.length());
+				}
+			}
+		}).perform();
+		
 		System.out.println("<testing-framework>");
 		testInterface(new SortMergeJoin());
 		System.out.println("</testing-framework>");
