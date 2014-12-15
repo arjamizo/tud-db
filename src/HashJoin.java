7fc336a1132933d364eac0f3b33ea2c7cc24ba5e
Artur Z <arjamizo@gmail.com>
2014-12-15 23:58:28 +0100

    assigning tuples to buckets

--- src/HashJoin.java
+++ src/HashJoin.java
@@ -21,15 +21,24 @@ public class HashJoin implements Join {
 
     public List<Triple> join(final List<Tuple> input1, final List<Tuple> input2) {
 	int size = 1 << 8;
-	ArrayList<LinkedList<Tuple> > buckets = new ArrayList(size);
+	ArrayList<LinkedList<Tuple> > buckets1 = new ArrayList(size);
+	ArrayList<LinkedList<Tuple> > buckets2 = new ArrayList(size);
 	for (int i = 0; i < size; i++) {
-	    buckets.add(new LinkedList());
+	    buckets1.add(new LinkedList());
+	    buckets2.add(new LinkedList());
 	}
-	
+	Tuple t;
 	for (int i = 0; i < input1.size(); i++) {
-	    Tuple t = input1.get(i);
+	    t = input1.get(i);
 	    int hash = hash(t.getID());
 	    System.out.printf("id=%d\thash=%d\n", t.getID(), hash);
+	    buckets1.get(hash).add(t);
+	}
+	
+	for (int i = 0; i < input2.size(); i++) {
+	    t = input2.get(i);
+	    int hash = hash(t.getID());
+	    buckets2.get(hash).add(t);
 	}
 	return null;
     }
